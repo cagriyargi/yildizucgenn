94e9d105d4754b66a29f5803252b2e20c9bdd32a
cagriyargi <49692049+cagriyargi@users.noreply.github.com>
2019-04-23 22:00:18 +0300

    Create ucgenn.c

---
+++ ucgenn.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+#include <conio.h>
+int main()
+{
+    
+int n,i;
+    
+for(int n=4;n>=0;n--){
+   
+for(int i=0;i<=n;i++){
+            
+printf("*");
+
+		}        
+printf("\n");
+	}
+}
